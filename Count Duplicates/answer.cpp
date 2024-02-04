29458744a329bea2ae538bb9230dfa1bbeaa3375
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2024-02-04 11:57:04 +0300

    Create answer.cpp

---
+++ Count Duplicates/answer.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+std::size_t duplicateCount(const std::string& in)
+{
+	size_t count = 0;
+	std::vector<char> checked;
+
+	for (int i = 0; i < in.length(); i++)
+	{
+		for (int j = 0; j < in.length(); j++)
+		{
+			char current = tolower(in[j]);
+
+			if (find(checked.begin(), checked.end(), current) != checked.end())
+			{
+				continue;
+			}
+
+			if (tolower(in[i]) == current && i != j)
+			{
+				count++;
+				checked.push_back(tolower(in[j]));
+				break;
+			}
+		}
+	}
+
+	return count;
+}
